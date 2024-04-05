13bd64bcee10d95e0413df3b4f5ff6b024f536a3
annisa :b <annisaputriaprilia12@gmail.com>
2024-04-05 21:35:04 +0700

    booleans in Kotlin

---
+++ src/lima - booleans.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val isKotlinFun: Boolean = true
+    val isFishTasty: Boolean = false
+    println(isKotlinFun)
+    println(isFishTasty)
+
+    val x = 5
+    val y = 7
+    println(x > y)
+    println(5 < 7)
+    println(x == 5)
+}
